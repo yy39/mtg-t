c363b280069a8be03c50c31862860087af27034c
Yusan Yin <yusanyin@gmail.com>
2024-02-24 20:56:43 -0800

    create numOf dict

--- nazgulCalculator.py
+++ nazgulCalculator.py
@@ -1,5 +1,11 @@
 import pyautogui
 
 test = pyautogui.prompt(text='', title='Test' , default='')
+numOf = {
+  'nonToken': 0,
+  'Token': 0,
+  'tokenDoubler': 0,
+  'counterDoubler': 0
+}
 
 print(test)
