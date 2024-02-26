9da84bee410c7abdcd5bccd848f19d7872e01e8b
yy39 <yusanyin@gmail.com>
2024-02-25 22:16:14 -0800

    validate Input placeholder

--- nazgulCalculator.py
+++ nazgulCalculator.py
@@ -1,11 +1,17 @@
 import pyautogui
 
-test = pyautogui.prompt(text='', title='Test' , default='')
+test = pyautogui.prompt(text='Number of nonToken Nazgul', title='Test' , default='')
 numOf = {
   'nonToken': 0,
   'Token': 0,
   'tokenDoubler': 0,
   'counterDoubler': 0
 }
+
+def validateInput(input):
+    try:
+        print(input)
+    except:
+        print(input)
 
 print(test)
