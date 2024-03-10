0260063592c368a756e2873762f7e8465273962d
Yusan Yin <yusanyin@gmail.com>
2024-03-09 20:20:19 -0800

    add initial calculate func

--- nazgulCalculator.py
+++ nazgulCalculator.py
@@ -13,9 +13,14 @@ def validateInput(string: input):
     except:
         print(input)
 
-numOf['nonToken'] = pyautogui.prompt(text='Enter number of non-token Nazguls entering', title='Enter numbers 1/4', default='')
-numOf['token'] = pyautogui.prompt(text='Enter number of token Nazguls entering', title='Enter numbers 2/4', default='')
-numOf['tokenDoubler'] = pyautogui.prompt(text='Enter number of token doublers', title='Enter numbers 3/4', default='')
-numOf['counterDoubler'] = pyautogui.prompt(text='Enter number of counter doublers', title='Enter numbers 4/4', default='')
+def calculate(nums: dict):
+    for x in nums:
+        print(nums[x])
 
-print(numOf)
+# numOf['nonToken'] = pyautogui.prompt(text='Enter number of non-token Nazguls entering', title='Enter numbers 1/4', default='')
+# numOf['token'] = pyautogui.prompt(text='Enter number of token Nazguls entering', title='Enter numbers 2/4', default='')
+# numOf['tokenDoubler'] = pyautogui.prompt(text='Enter number of token doublers', title='Enter numbers 3/4', default='')
+# numOf['counterDoubler'] = pyautogui.prompt(text='Enter number of counter doublers', title='Enter numbers 4/4', default='')
+
+# print(numOf)
+calculate(numOf)
